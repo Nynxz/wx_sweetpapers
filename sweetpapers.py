d75fc3ec0e75da5bd6121eadf2a9aa41e61b446f
Nynxz <contact@nynxz.com>
2025-01-18 03:20:24 +1000

    better transition configuration

--- sweetpapers.py
+++ sweetpapers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import subprocess
 import json
 import argparse
@@ -6,23 +7,47 @@ import random
 import time
 
 
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 def change_wallpaper(config):
     path = os.path.expandvars(config["defaults"].get("packs_location"))
-    directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
+    # Transitions
+    t_type = config["defaults"].get("transition_type")
+    t_duration = config["defaults"].get("transition_duration")
+    t_step = config["defaults"].get("transition_step")
+    t_fps = config["defaults"].get("transition_fps")
+    fill_mode = config["defaults"].get("fill_mode")
+    directories = [d for d in os.listdir(
+        path) if os.path.isdir(os.path.join(path, d))]
     print(directories)
+    last_d = ""
     while True:
-        d = random.choice(directories)
-        # for d in directories:
-        print(d)
+        if len(directories) > 1:
+            last_d = random.choice([d for d in directories if d != last_d])
+        else:
+            last_d = directories[0]
         imgs = [
-            (f.split(".")[0], os.path.join(path, d, f))
-            for f in os.listdir(os.path.join(path, d))
+            (f.split(".")[0], os.path.join(path, last_d, f))
+            for f in os.listdir(os.path.join(path, last_d))
         ]
-        for i in imgs:
-            monitor = config["screens"].get(i[0])
-            print(monitor)
-            print(i[1])
-            command = f"swww img -o {monitor} {i[1]} --transition-type center"
+        for screen, img in imgs:
+            monitor = config["screens"].get(screen)
+            command = f"swww img -o {monitor} {img} --resize {
+                fill_mode
+            } --transition-type {t_type} --transition-duration {
+                t_duration
+            } --transition-step {t_step} --transition-fps {t_fps}"
             try:
                 result = subprocess.run(
                     command,
@@ -32,12 +57,21 @@ def change_wallpaper(config):
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                 )
-
-                print("Output: ", result.stdout)
-                print("Errors: : ", result.stderr)
+                print(
+                    f"{bcolors.OKGREEN}Swapped Screen {
+                        monitor} to {img} {bcolors.ENDC}"
+                )
+                if result.stdout:
+                    print("Output: ", result.stdout)
+                if result.stderr:
+                    print("Errors: : ", result.stderr)
             except subprocess.CalledProcessError as e:
-                print(f"Command failed with errors: {e}")
-        time.sleep(10)
+                print(
+                    f"Command failed with errors: {e}\n {
+                        bcolors.WARNING
+                    } Is swww-daemon running? {bcolors.ENDC}"
+                )
+        time.sleep(config["defaults"].get("interval"))
 
 
 def debug_startup(config_data):
@@ -55,11 +89,13 @@ def debug_startup(config_data):
 def main():
     # Parse Arguments
     parser = argparse.ArgumentParser(description="wx_sweetpapers")
-    parser.add_argument("-c", "--config", help="Path to the custom config file")
+    parser.add_argument(
+        "-c", "--config", help="Path to the custom config file")
 
     args = parser.parse_args()
     if not args.config:
-        parser.error("Please provide the path to the config file using -c flag")
+        parser.error(
+            "Please provide the path to the config file using -c flag")
 
     with open(args.config, "r") as config_file:
         try:
@@ -77,4 +113,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting wx_sweetpapers")
