78f03195501c876867eaf729188e7003077de592
Nynxz <contact@nynxz.com>
2025-02-11 00:16:21 +1000

    WIP: progress towards v/h auto detection

--- sweetpapersauto.py
+++ sweetpapersauto.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import imageio.v3 as iio
+
 import subprocess
 import json
 import argparse
@@ -6,7 +8,6 @@ import os
 import random
 import time
 import signal
-import re
 from utils.colors import bcolors, wx_log
 from collections import defaultdict
 from dataclasses import dataclass
@@ -34,16 +35,17 @@ def get_transition_config(config):
 
 
 def construct_swww_cmd(tdata: TransitionData, monitor, img):
-    command = f"swww img -o {monitor} {img} --resize {
+    command = f"swww img -o {monitor} '{img}' --resize {
         tdata.tfill_mode
     } --transition-type {tdata.ttype} --transition-duration {
         tdata.tduration
     } --transition-step {tdata.tstep} --transition-fps {tdata.tfps}"
+    print(command)
     return command
 
 
 def swap_monitor_background(tdata, config, screen, img):
-    monitor = config["screens"].get(screen)
+    monitor = config["screens"].get(str(screen))
     command = construct_swww_cmd(tdata, monitor, img)
     try:
         result = subprocess.run(
@@ -67,33 +69,44 @@ def swap_monitor_background(tdata, config, screen, img):
         )
 
 
-def change_wallpaper(config, profile):
-    path = os.path.expandvars(config["defaults"].get("packs_location"))
-    if profile is not None:
+def change_wallpaper(config, profile, path=None):
+    if path is not None:
+        path = path
+    else:
+        path = os.path.expandvars(config["defaults"].get("packs_location"))
         path = os.path.join(path, profile)
-
+        print(path)
+    print("HELLO")
+    print(path)
     tdata = get_transition_config(config)
 
     directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
     directories.sort()
+    print(directories)
     wx_log("STARTING WX_SWEETPAPERS", bold=True, underline=True)
     wx_log(
         f"Found {len(directories)} Directories in {profile} \n- {'- '.join([f'{path}/{x}\n' for x in directories])}"
     )
 
     last_d = directories[0]
-    last_d_i = 1
+    last_d_i = 0
 
-    img_path = os.path.join(path, last_d)
-    imgs = [(f.split(".")[0], os.path.join(img_path, f)) for f in os.listdir(img_path)]
-    wx_log(f"\nSwapping to {img_path}", bold=True, underline=True)
-    imgs = pick_random_image_from_alphapack(imgs, config)
-
+    # img_path = os.path.join(path, last_d)
+    # imgs = [(f.split(".")[0], os.path.join(img_path, f)) for f in os.listdir(img_path)]
+    # imgs = [(f.split(".")[0], os.path.join(img_path, f)) for f in os.listdir(img_path)]
+    # wx_log(f"\nSwapping to {img_path}", bold=True, underline=True)
+    # imgs = pick_random_image_from_alphapack(imgs, config)
+    # print(imgs)
     # Quick Swap all monitors
-    for i in range(len(imgs)):
-        screen, img = imgs[i]
-        swap_monitor_background(tdata, config, screen, img)
-    time.sleep(config["defaults"].get("interval"))
+    # for i in range(len(imgs)):
+    #    print(imgs[i])
+    #    screen, img = imgs[i]
+    #    screen = config["screens"].get(screen)
+    #    print(config["screens"].get("1"))
+    #   print(screen)
+    #   print(img)
+    #   swap_monitor_background(tdata, config, screen, str(img))
+    # time.sleep(config["defaults"].get("interval"))
 
     while True:
         if len(directories) > 1:
@@ -118,7 +131,10 @@ def change_wallpaper(config, profile):
             continue
         imgs = pick_random_image_from_alphapack(imgs, config)
         # Swap Monitors
+        print(imgs)
         for screen, img in imgs:
+            print(screen, img)
+            print(config["screens"].get(str(screen)))
             swap_monitor_background(tdata, config, screen, img)
             # Swap Monitors 1 at a time
             if config["defaults"].get("sequence") != "true":
@@ -127,25 +143,61 @@ def change_wallpaper(config, profile):
             time.sleep(config["defaults"].get("interval"))
 
 
+def is_landscape(path):
+    result = subprocess.run(
+        ["exiftool", "-json", path], stdout=subprocess.PIPE, text=True
+    )
+
+    # Parse the JSON output
+    metadata = json.loads(result.stdout)[0]
+
+    # Extract resolution
+    width = metadata.get("ImageWidth")
+    height = metadata.get("ImageHeight")
+    # height, width = iio.imread(path).shape[:2]
+    return height < width
+
+
 def pick_random_image_from_alphapack(imgs, config):
+    print("Picking images")
     groups = defaultdict(list)
-    for name, path in imgs:
-        match = re.match(r"(\d+)", name)  # Extract numeric prefix
-        if match:
-            # cleaned_name = re.sub(r"[a-zA-Z]", "", name)
-            index = int(match.group(1))  # Convert to integer
-            groups[index].append((str(index), path))
+
+    for _, path in imgs:
+        orientation = "Landscape" if is_landscape(path) else "Portrait"
+        groups[orientation].append(path)
+        # match = re.match(r"(\d+)", name)  # Extract numeric prefix
+        # if match:
+        # cleaned_name = re.sub(r"[a-zA-Z]", "", name)
+        #     index = int(match.group(1))  # Convert to integer
+        #    orientation = groups["Landscape" if is_landscape(path) else "Portrait"].append((str))
+        #    groups[index].append((str(index), path))
+
+    # print(groups)
     # wx_log(
     #    f"{'\n'.join([f'{key} {config["screens"].get(str(key))} : \n-{"".join(["\n-".join([y[1] for y in value])])}' for key, value in groups.items()])}"
     # )
-    wx_log("Possibilities", underline=True)
+    # wx_log("Possibilities", underline=True)
 
-    wx_log(
-        f"{'\n'.join([f'{config["screens"].get(str(key))} : {len(value)}' for key, value in groups.items()])}"
-    )
-    selected_files = [random.choice(group) for group in groups.values()]
+    # wx_log(
+    #    f"{'\n'.join([f'{config["screens"].get(str(key))} : {len(value)}' for key, value in groups.items()])}"
+    # )
+
+    # wx_log(
+    #   f"{'\n'.join([f'{config["screens"].get(str(key))} : {["Landscape" if is_landscape(z[1]) else "Portrait" for z in value]}' for key, value in groups.items()])}"
+    # )
+
+    landscape = 1
+    portrait = 2
+
+    selected_files = [
+        (2, random.choice(groups["Portrait"])),
+        (1, random.choice(groups["Landscape"])),
+        (3, random.choice(groups["Portrait"])),
+    ]
+    # selected_files = [random.choice(group) for group in groups.values()]
     # custom order (R(2) > Middle(0) > L(1))
 
+    print(selected_files)
     # Sort 1-3
     try:
         selected_files = sorted(selected_files, key=lambda x: x[0], reverse=True)
@@ -155,11 +207,12 @@ def pick_random_image_from_alphapack(imgs, config):
         wx_log(f"Failed swap: {e}", bcolors.FAIL)
     # Reverse Order
     # selected_files = selected_files[::-1]
+    print("Picked images")
 
     wx_log("SELECTED", underline=True)
-    wx_log(
-        f"{'\n'.join([f'{"".join([":".join([y for y in value])])}' for value in selected_files])}"
-    )
+    # wx_log(
+    #   f"{'\n'.join([f'{"".join([":".join([y for y in value])])}' for value in selected_files])}"
+    # )
     return selected_files
 
 
@@ -211,15 +264,18 @@ def load_config():
     parser = argparse.ArgumentParser(description="wx_sweetpapers")
     parser.add_argument("-c", "--config", help="Path to the custom config file")
     parser.add_argument("-p", "--profile", help="Path to the wallpaper pack profile")
+    parser.add_argument("-P", "--path", help="Path to the wallpaper pack profile")
     args = parser.parse_args()
     if not args.config:
         parser.error("Please provide the path to the config file using -c flag")
     if not args.profile:
         args.profile = None
+    if not args.path:
+        args.path = None
 
     with open(args.config, "r") as config_file:
         try:
-            return (json.load(config_file), args.profile)
+            return (json.load(config_file), args.profile, args.path)
         except json.JSONDecodeError as e:
             print(f"Error parsing Config File: {e}")
             exit()
@@ -227,9 +283,9 @@ def load_config():
 
 def main():
     shutdown_already_running()
-    [config_data, profile] = load_config()
+    [config_data, profile, path] = load_config()
     debug_startup(config_data)
-    change_wallpaper(config_data, profile)
+    change_wallpaper(config_data, profile, path)
 
 
 if __name__ == "__main__":
---
+++ wofi_menu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BASE_DIR="$HOME/Wallpapers/packs/"
+
+process_name="swww-daemon"
+process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n", $2/1024, $3}')
+
+directories=$(find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d)
+selected_dir=$(echo -e "$directories\n$process_info" | wofi --show dmenu --insensitive true --prompt "Select a Wallpaper Pack")
+
+if [[ "$selected_dir" == "$process_info" ]]; then
+  process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n",$2/1024, $3}')
+  # Kill Running swww-daemon
+  notify-send "Before: $process_info"
+  #echo "$process_info" | awk '{print $1}' | xargs -r kill
+
+  pgrep "swww-daemon" | xargs kill
+  # Dispatch new swww-daemon
+  hyprctl dispatch exec swww-daemon
+
+  # Get Updated Memory Usage
+  process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n", $2/1024, $3}')
+  notify-send "-a" "yes" "After: $process_info"
+elif [ -d "$selected_dir" ]; then
+  selected_dir=$(basename "$selected_dir")
+  echo "You Selected: $selected_dir"
+  /home/user/.config/wx_sweetpapers/sweetpapers.py -c /home/user/.config/wx_sweetpapers/sweetpapers.jsonc -p "$selected_dir" &
+  notify-send "$selected_dir" "$process_info"
+else
+  notify-send "❔No directory selected."
+fi
