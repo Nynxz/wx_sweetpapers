a46e3be3e570fcd58a6b588cc0236c7faea9d637
Nynxz <contact@nynxz.com>
2025-02-15 23:12:40 +1000

    update wofi_menu.sh

--- extras/wofi_menu.sh
+++ extras/wofi_menu.sh
@@ -6,26 +6,37 @@ process_name="swww-daemon"
 process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n", $2/1024, $3}')
 
 directories=$(find "$BASE_DIR" -mindepth 1 -maxdepth 1 \( -type d -o -type l \))
-selected_dir=$(echo -e "$directories\n$process_info" | wofi --show dmenu --insensitive true --prompt "Select a Wallpaper Pack")
 
-if [[ "$selected_dir" == "$process_info" ]]; then
-  process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n",$2/1024, $3}')
-  # Kill Running swww-daemon
-  notify-send "Before: $process_info"
-  #echo "$process_info" | awk '{print $1}' | xargs -r kill
+commands="$directories"
 
+if [ -n "$process_info" ]; then
+  commands="$directories\n$process_info\nKill swww-daemon"
+else
+  commands="$commands\nStart swww-daemon"
+fi
+
+selected_option=$(echo -e "$commands" | wofi --show dmenu --insensitive true --prompt "Select a Wallpaper Pack")
+
+if [[ "$selected_option" == "$process_info" ]] && [ -n "$process_info" ]; then
   pgrep "swww-daemon" | xargs kill
-  # Dispatch new swww-daemon
-  hyprctl dispatch exec swww-daemon
-
   # Get Updated Memory Usage
-  process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n", $2/1024, $3}')
-  notify-send "-a" "yes" "After: $process_info"
-elif [ -d "$selected_dir" ]; then
-  selected_dir=$(basename "$selected_dir")
-  echo "You Selected: $selected_dir"
-  /home/user/.config/wx_sweetpapers/src/sweetpapers.py -c /home/user/.config/wx_sweetpapers/sweetpapers.jsonc -p "$selected_dir" &
-  notify-send "$selected_dir" "$process_info"
+  new_process_info=$(ps -eo pid,rss,comm | grep "$process_name" | grep -v "grep" | awk '{printf "%.2fMB %s\n", $2/1024, $3}')
+  notify-send "♻️ Restarting swww-daemon" "Before $process_info\nAfter $new_process_info"
+  hyprctl dispatch exec swww-daemon
+elif [[ "$selected_option" == "Kill swww-daemon" ]]; then
+  notify-send "🔴 Killing swww-daemon"
+  pkill 'swww-daemon'
+elif [[ "$selected_option" == "Start swww-daemon" ]]; then
+  notify-send "🟢 Starting swww-daemon"
+  hyprctl dispatch exec swww-daemon
+elif [ -d "$selected_option" ]; then
+  selected_option=$(basename "$selected_option")
+  if [ -z "$process_info" ]; then
+    process_info="Starting..."
+    hyprctl dispatch exec swww-daemon
+  fi
+  /home/user/.config/wx_sweetpapers/src/sweetpapers.py -c /home/user/.config/wx_sweetpapers/sweetpapers.jsonc -p "$selected_option" &
+  notify-send "🟢 $selected_option" "$process_info"
 else
   notify-send "❔No directory selected."
 fi
